09cc5f2c2652bb4d1929de65967ab8b84838c235
paulaetgr92 <paulaetgr92@gmail.com>
2025-04-28 13:29:59 -0300

    fix: prepare app for Render deploy

--- config.py
+++ config.py
@@ -2,20 +2,24 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
 
-
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'minha_chave_secreta')
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
-        'DATABASE_URL', 
-        'sqlite:///banco.db'  #
+        'DATABASE_URL',
+        'sqlite:///banco.db'
     )
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
 
     return app
+
+
+if __name__ == "__main__":
+    app = create_app()
+    port = int(os.environ.get("PORT", 5000)) 
+    app.run(host="0.0.0.0", port=port, debug=True)
